ca7a7ec3450b7ee9beb20613ce42eff58050728c
Igor da Silva Dias <rogigor1712@gmail.com>
2026-07-27 10:58:41 -0300

    adiciona animação ao fundo

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,12 @@ import Sobre from "./components/sobre";
 import Contatos from "./components/contato";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
+import Background from "./components/background";
 
 export default function Page() {
     return (
         <>
+            <Background />
             <Navbar />
             <Hero />
             <Projetos />
